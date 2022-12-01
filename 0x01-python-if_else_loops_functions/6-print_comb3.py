24b67cf8c9e2350553387d381a58c425f3c48770
youngspeculator <jayypalll@gmail.com>
2022-12-01 04:28:26 -0800

    a program that prints all possible different combinations of two digits.The two digits must be different.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for x in range(0, 10):
-        if (i<x):
-            print("{}{}".format(i,x), end="")
+        if (i < x):
+            print("{}{}".format(i, x), end="")
             if (i < 8):
                 print(", ", end="")
 print("\n", end="")
