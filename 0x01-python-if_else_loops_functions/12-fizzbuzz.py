4584650004d3c0d7b10c8a1dfbb091286c50fe92
youngspeculator <jayypalll@gmail.com>
2022-12-02 05:12:59 -0800

    prints 1 to 100 separated by a space. for multiples of 3 print Fizz, for multiples of 5  print Buzz, for multiples of both 3 and 5 print FizzBuzz.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 15 == 0):
-            print("FizzBuzz", end="")
+            print("FizzBuzz ", end="")
         elif (i % 5 == 0):
-            print("Buzz", end="")
+            print("Buzz ", end="")
         elif (i % 3 == 0):
-            print("Fizz", end="")
+            print("Fizz ", end="")
         else:
-            print(i, end="")
+            print("{} ".format(i), end="")
 
