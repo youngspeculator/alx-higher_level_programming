6613ff2850026552ce7e76788ee76e6d5ccd702c
youngspeculator <jayypalll@gmail.com>
2022-12-06 01:17:09 -0800

    a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,11 +11,12 @@ def arguments():
     if (length - 1 >= 0):
         c_or_d = ":"
 
-    print ("{} {}{}".format(length, arg_string, c_or_d,))
+    print("{} {}{}".format(length, arg_string, c_or_d,))
 
-    for index, arg in enumerate (argv):
+    for index, arg in enumerate(argv):
         if (index > 0):
             print("{}: {}".format(index, arg))
 
+
 if __name__ == "__main__":
-        arguments()
+    arguments()
