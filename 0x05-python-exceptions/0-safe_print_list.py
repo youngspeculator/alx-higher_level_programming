f29ed75f80c2be9edca40f5226f0afb2b7ee90e2
youngspeculator <jayypalll@gmail.com>
2022-12-20 02:44:33 -0800

    a function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-""" 
-my_list can contain any type (integer, string, etc.)
-all elements must be printed on the same line
-followed by a new line.
-@x: the number of elements to print
-    x can be > len(my_list)
-Return: real number of elements printed
-"""
+
+
 def safe_print_list(my_list=[], x=0):
+
     total = 0
     for i in range(x):
         try:
@@ -17,5 +12,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print()
     return(total)
-
-
