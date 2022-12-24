8b2a334474ab8b3df4eb311e67102ab38b8bbbc7
youngspeculator <jayypalll@gmail.com>
2022-12-24 02:13:08 -0800

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,40 +1,52 @@
 #!/usr/bin/python3
+class Square():
+    """
+        Defining a Square
+    """
 
-"""Define a class Square."""
-
-
-class Square:
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+    def __init__(self, size=0):
+        """Initialization of instance attributes
+            Args:
+            size (int): Zero or positive number.
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculares the area
+            
+            Return: The current square area.
+        """
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """Updating the private attributes
+            Args:
+            value (int): Zero or positive number.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
-
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="")] for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")
+       """
+            prints in stdout the square with the character # or a new line
+            is size is zero.
+       """
+       if self.__size == 0:
+           print()
+           return
+       for col in range(self.__size):
+           for row in range(self.size):
+               print("#", end ="")
+               print()
