676ec21e48d455521f239e3b6cebe86320be0904
youngspeculator <jayypalll@gmail.com>
2024-05-27 09:59:25 +0000

    make sure the code is 3 lines long

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-# complete the code challenge
-# print the int stored in the var, followed by `Battery Street`
-# followed by a new line    
-# not allowed to cast the variable into a string
-# code must be 3 lines long
-# must use f-strings
-
 number = 98
 print(f"{number:d} Battery street".format(number))
