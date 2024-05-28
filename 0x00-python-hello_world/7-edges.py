70fb35f41a57810441e4d5b3d6dad80196e4b014
youngspeculator <jayypalll@gmail.com>
2024-05-28 11:24:39 +0000

    syntax change

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[0:3]
 middle_world = word[1:8]
 word_last = word[7:9]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print("First 3 letters: {word_first_3}")
+print("Last 2 letters: {word_last_2}")
+print("Middle word: {middle_word}")
