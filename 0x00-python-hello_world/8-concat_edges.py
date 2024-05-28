7804a18bfe0797fff8afd39b3f309a7553c0a77b
youngspeculator <jayypalll@gmail.com>
2024-05-28 16:31:45 +0000

    will the output change after edit to str

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clean syntax"
+language that combines remarkable power with very clean syntax"
 str = str[39:66] + str[106:112] + str[:6] + '\n'
 print(str)
