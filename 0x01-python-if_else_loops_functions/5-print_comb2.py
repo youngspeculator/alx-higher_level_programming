50efe4ab7a531fced0a7996713e04e88b8be5d64
youngspeculator <jayypalll@gmail.com>
2024-05-30 17:42:28 +0300

    more changes to 5-.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number != 99:
         print("{}, ".format(int(number)), end="")
     else:
-        print(f"{}".format(int(number)))
+        print("{}".format(int(number)))
