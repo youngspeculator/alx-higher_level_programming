1c227068fc6b6966f71edbdd4bfe4613fbde4c33
youngspeculator <jayypalll@gmail.com>
2024-05-31 13:17:51 +0300

    invalid output: no need to account for invalid input

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,5 +12,3 @@ elif last_digit == 0:
 elif last_digit < 6 and last_digit != 0:
     print("Last digit of {} is {} and is less than 6 and \
 not 0".format(number, last_digit))
-else:
-    print("Invalid input")
