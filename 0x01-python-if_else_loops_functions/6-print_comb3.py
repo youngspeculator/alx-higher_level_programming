df3f74c2e03c6f7e2f3ff67beedfac90bafa3c59
youngspeculator <jayypalll@gmail.com>
2024-05-31 14:07:05 +0300

    resolving pedantic

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 for digit1 in range(0, 10):
     for digit2 in range(0, 10):
-        if (digit1 < digit2):            
+        if (digit1 < digit2):
             if (digit1 == 8 and digit2 == 9):
                 print("{}{}".format(digit1, digit2), end=" ")
             else:
                 print("{}{}".format(digit1, digit2), end=", ")
-print()
+print()                
+
+
