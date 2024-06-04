57d819b9be186fcccfe55965fa29f2a6ddbf495d
youngspeculator <jayypalll@gmail.com>
2024-06-04 16:19:54 +0300

    fixed the indentation errors

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	if (number < 0):
-		number = number * -1
-	print(number % 10, end="")
-	return (number % 10)
+    if number < 0:
+        number = number * -1
+    print(number % 10, end="")
+    return number % 10  # Correctly indented return statement
